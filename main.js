b1dc4b055bc93a57acff9b5c6f16f2f1dedf3e7a
cordellr <cordell.rainock@gmail.com>
2019-01-20 21:46:39 -0600

    Finished. Creates dice, rolls all, sums all, single/double click on die

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
 $(document).ready(function () {
 
+    let diceArray = [];
+
     class Die {
         constructor(value) {
-            this.value = value
+            //gets value from roll function when new die is created
+            this.value = value;
+            //created die with value inserted
             this.div = $('<div class="dice-box"></div>');
             $("#dice-container").append(this.div);
             $(this.div).text(value);
+
+            //adds die to array, helpful in working with all dice
+            diceArray.push(this);
+
+            //performs roll on specific die clicked
+            $(this.div).click(() => {
+                this.value = roll();
+                $(this.div).text(this.value);
+            });
+
+            //removes dice by removing div and the associated value from the array, so that sum can be properly calculated
+            $(this.div).dblclick(() => {
+                $(this.div).remove();
+                diceArray.splice($.inArray(this, diceArray),1);
+            });
+
+
         }
     }
 
+    //generates a new Die class object and assigns it a value
     $("#btn-generate").click(function () {
-        let value = randomValue();
+        let value = roll();
         let die = new Die(value);
     })
 
-    let randomValue = function() {
+    //calculates random value between 1 and 6
+    let roll = function () {
         return Math.floor(Math.random() * 6 + 1);
     }
 
+    //Resets all current dice with new random values
+    $("#btn-rollAll").click(function () {
+        for (let i = 0; i < diceArray.length; i++) {
+            //rolls dice, new value and text to match that value
+            let diceValue = roll();
+            diceArray[i].div.text(diceValue);
+            diceArray[i].value = diceValue;
+        }
+    })
+
+    //button sums values of all dice, displays alert
+    $("#btn-sum").click(function () {
+        let sum = 0;
+        for (let i = 0; i < diceArray.length; i++) {
+            sum += diceArray[i].value;
+        }
+        alert(`The sum of all dice is ${sum}.`)
+    });
 
 });
